38ac2d045cf1147243a70b5438e7efc8f36b748d
gianluca <alex992palermo@gmail.com>
2023-12-19 17:45:25 +0100

    Aggiunto Esercizio Operatore Ternario2

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println("Valore: " + i);
+            System.out.println(checkValue(i));
+        }
+    }
+
+    /*private static String checkValue(int value) {
+        String result = "";
+
+        if (value % 5 == 0) {
+            result = "FizzBuzz";
+        } else  {
+            result = "Buzz";
+        }
+        return result;
+    }*/
+    private static String checkValue(int value){
+        String result;
+        result = value % 5 == 0 ? "FizzBuzz" : "Buzz";
+        return result;
+    }
+}
